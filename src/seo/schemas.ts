4dcea18ef2ca66491c5de5949aecc59560b56fbc
nickthx <whitty6417@gmail.com>
2026-01-18 12:14:30 -0500

    Added SCHEMA-IMPLEMENTATION-SUMMARY.md

---
+++ src/seo/schemas.ts
@@ -0,0 +1,45 @@
+/**
+ * Schema.org structured data definitions for DTE Roofing
+ */
+
+export const localBusinessSchema = {
+  "@context": "https://schema.org",
+  "@type": "RoofingContractor",
+  "@id": "https://dteroofing.com/#business",
+  "name": "DTE Roofing",
+  "url": "https://dteroofing.com/",
+  "telephone": "+1-614-971-6028",
+  "priceRange": "$$",
+  "image": "https://www.dteroofingllc.com/DTE-Roofing-Logo-two-Men.png",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "615 Hilliard Rome Rd",
+    "addressLocality": "Columbus",
+    "addressRegion": "OH",
+    "postalCode": "43228",
+    "addressCountry": "US"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 39.9637153,
+    "longitude": -83.1477371
+  },
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": ["Monday","Tuesday","Wednesday","Thursday","Friday"],
+      "opens": "08:00",
+      "closes": "18:00"
+    },
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": "Saturday",
+      "opens": "10:00",
+      "closes": "14:00"
+    }
+  ],
+  "sameAs": [
+    "https://www.facebook.com/people/DTE-Roofing/61556271692460/",
+    "https://www.instagram.com/dte_roofing/"
+  ]
+};
